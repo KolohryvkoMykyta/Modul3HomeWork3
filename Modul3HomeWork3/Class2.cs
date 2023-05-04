9322b41296c0262fb6c25a368a6fddbdbe89e4d5
Kolohryvko Mykyta <nkologhrivko@gmail.com>
2023-05-04 18:49:44 +0300

    finished home work 3

---
+++ Modul3HomeWork3/Class2.cs
@@ -0,0 +1,19 @@
+﻿namespace Modul3HomeWork3
+{
+    public class Class2
+    {
+        private int _powResult;
+
+        public Func<int, bool> Calc(Func<int, int, int> pow, int value1, int value2)
+        {
+            _powResult = pow(value1, value2);
+
+            return Result;
+        }
+
+        public bool Result(int value)
+        {
+            return _powResult % value == 0;
+        }
+    }
+}
--- Modul3HomeWork3/Program.cs
+++ Modul3HomeWork3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul3HomeWork3;
+
+public class Program
+ {
+    public static void Show(bool result)
+    {
+        Console.WriteLine(result);
+    }
+
+    private static void Main(string[] args)
+    {
+        var class1 = new Class1();
+        var class2 = new Class2();
+        var result = class2.Calc(class1.Pow, 12, 29);
+
+        class1.ShowHandler(result(10000));
+    }
+}
